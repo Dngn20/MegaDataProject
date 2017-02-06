edbd3d5f16c43a9e9442a3029bbd6f102dc29197
Dngn20 <dngn20@gmail.com>
2017-02-06 13:23:54 -0700

    Initial Commit

---
+++ MegaDataProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MegaDataProject
+//
+//  Created by Nguyen, Duncan on 2/6/17.
+//  Copyright © 2017 Nguyen, Duncan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
