771010acaf96ca6d63c41a6dc57fee0de2a30efc
Dngn20 <dngn20@gmail.com>
2017-02-08 13:52:33 -0700

    added get size and method

--- MegaDataProject/Controller/DataStructureController.cpp
+++ MegaDataProject/Controller/DataStructureController.cpp
@@ -37,4 +37,16 @@ void DataStructureController :: testIntArray()
         cout << temp.getFromIndex(index) << " is at spot " << index << endl;
     }
     
+    cout << "testing input" << endl;
+    
+    for(int index = 0; index < 3; index++)
+    {
+        temp.setAtIndex( index, index);
+    }
+    
+    for(int index = 0; index < temp.getSize(); index++)
+    {
+        cout << temp.getFromIndex(index) << " is at spot "<< index << endl;
+    }
+    
 }
