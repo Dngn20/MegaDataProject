bda16bf497079d128119e25e81893a522fbdd75c
Dngn20 <dngn20@gmail.com>
2017-04-28 12:06:10 -0600

    Added to AVL Tree and binary search tree

--- MegaDataProject/AVLTree.hpp
+++ MegaDataProject/AVLTree.hpp
@@ -16,7 +16,7 @@ template <class Type>
 class AVLTree : public BinarySearchTree<Type>
 {
 private:
-    BinarySearchTree<Type> * leftRotation (BinarySearchTreeNode<Node> * parent);
+    BinarySearchTree<Type> * leftRotation (BinarySearchTreeNode<Type> * parent);
     BinarySearchTree<Type> * rightRotation (BinarySearchTree<Type> * parent);
     BinarySearchTree<Type> * leftRightRotation(BinarySearchTree<Type> * parent);
     BinarySearchTree<Type> * rightLeftRotation(BinarySearchTree<Type> * parent);
@@ -35,6 +35,18 @@ public:
     void insert(Type itemToInsert);
     void remove(Type value);
 };
+
+template <class Type>
+AVLTree<Type> :: AVLTree() : BinarySearchTreeNode<Type>()
+{
+    this->root = nullprt
+}
+
+template <class Type>
+AVLTree<Type> :: ~AVLTree()
+{
+    delete this-getRoot;
+}
 
 template <class Type>
 int AVLTree<Type> :: heightDifference(BinarySearchTreeNode<Type> * node)
@@ -94,4 +106,61 @@ BinarySearchTreeNode<Type> * AVLTree<Tree> :: leftRightRotation(BinarysearchTree
     return leftRotation(parent);
 }
 
+template <class Type>
+BinarySearchTreeNode<Type> * AVLTree :: balanceSubTree (BinarySearchTreeNode<Type> * parent)
+{
+    int balanceFactor = heightDifferene(parent);
+    
+    if(balanceFactor > 1)
+    {
+        if(heightDifference(parent-.getLeftChild()) > 0)
+        {
+            parent = leftRotation(parent);
+        }
+        else
+        {
+            parent = leftRightRotation(parent);
+        }
+    }
+    else if(balanceFactor < -1)
+    {
+        if(heightDifference(parent->getRightChild()) > 0)
+        {
+            parent = rightLeftRotation(parent);
+        }
+        else
+        {
+            parent = rightRotation(parent);
+        }
+    }
+    return parent;
+}
+
+template <class Type>
+BinarySearchTreeNode<Type> * AVLTree<Type> :: insertNode(BinarySearchTreeNode<Type> * parent, Type inserted)
+{
+    if(parent == nullptr)
+    {
+        parent = new BinarySearchTreeNode<Type>(inserted);
+        return parent
+    }
+    else if(inserted < parent->getNodeData())
+    {
+        parent->setRightChild(insertNoe(parent->getRightChild(), inserted));
+    }
+    return parent;
+}
+
+template <class Type>
+void AVLTree :: insert(type item)
+{
+    insertNode(this->getRoot(), item);
+}
+
+template <class Type>
+void AVLTree<Type> :: remove(Type item)
+{
+    
+    removeNode(this->getRoot(), item);
+}
 #endif /* AVLTree_h */
